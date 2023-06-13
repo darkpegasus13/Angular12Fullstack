83402b15058342d772d5c7c405791e428180063a
darkpegasus13 <jayeshbhushan1309@gmail.com>
2023-06-13 17:02:21 +0530

    Code refractored and reactive forms impemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LocationService } from './location.service';
+import { LocationService } from './Services/location.service';
 import { HttpClientModule } from '@angular/common/http';
 import { LocationClassComponent } from './location-class/location-class.component';
 import { Routes, RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -20,13 +22,15 @@ const routes: Routes = [
     AppComponent,
     LandingPageComponent,
     LocationClassComponent,
-    LoginComponent
+    LoginComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule
   ],
   providers: [LocationService],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      userName: ["", Validators.required],
+      password: ["", [Validators.required, this.PasswordValidator]]
+    })
+  }
+  ngOnInit() {
+  }
+
+  PasswordValidator(control: AbstractControl) {
+    if (control.value.startsWith('Jayesh')) {
+      return { invalidPassword: true };
+    }
+    return null;
+  }
+
+  OnSubmit() {
+    console.log(this.loginForm);
+  }
 
 }
